d574fddfb018088d35bbc34aaced62efcd7218c5
amitkr24 <amitkr695@gmail.com>
2022-08-21 15:21:00 +0530

    change home link

--- issue-tracker/config/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/issueTracker');
-
-const db = mongoose.connection;
-db.on('error',console.error.bind(console, "Error connection to mongodb"));
-db.once('open',function(){
-    console.log('connected to database :: MongoDB');
-});
-module.exports = db;
--- issue-tracker/models/project.js
+++
@@ -1,4 +0,0 @@
-const mongoose      = require('mongoose')
-const projectSchema = new mongoose.Schema({
-    
-})
